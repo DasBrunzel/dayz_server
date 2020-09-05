7a6d03026b4a15c9d00bbf858a5241fd89d79351
DasBrunzel <brunowenzel83@gmail.com>
2020-09-05 23:31:40 +0200

    Update start_dayz_server_modserver.bat

--- start_dayz_server_modserver.bat
+++ start_dayz_server_modserver.bat
@@ -2,7 +2,7 @@
 cls
 set watch=DayZ-Expansion Server
 title %watch% Watchdog
-cd "F:\Eigenen Dateien\Desktop\dayz"
+cd "C:\PFAD\ZU\DayZServer_x64.exe"
 :watchdog
 echo (%time%) %watch% started.
 start "DayZ-Expansion Server" /wait "DayZServer_x64.exe" -config=serverDZ.cfg -port=2302 "-mod=@CF;@Community-Online-Tools;@DayZ-Expansion;@DayZ-Expansion-Licensed" -profiles=.\ServerProfile -netlog -adminlog -enableHT -malloc=tbbmalloc 
