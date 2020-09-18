642e6f2893b04d037772cec24c5cfc31a33321dc
DasBrunzel <brunowenzel83@gmail.com>
2020-09-18 11:13:48 +0200

    Update start_DayZServer_Livonia.bat

--- start_DayZServer_Livonia.bat
+++ start_DayZServer_Livonia.bat
@@ -5,6 +5,6 @@ title %watch% Watchdog
 cd "Z:\DayZServer_Livonia"
 :watchdog
 echo (%time%) %watch% started.
-start "DayZ-Expansion Server" /wait "DayZServer_x64.exe" -config=serverDZ.cfg -port=2402 "-mod=mods\@CF;mods\@Community-Online-Tools;mods\@DayZ-Expansion;mods\@DayZ-Expansion-Licensed;mods\@Trader;mods\@TraderWalletCollection;mods\@gglaw_Fairlane500" -profiles=.\ServerProfile -netlog -adminlog -enableHT -malloc=tbbmalloc 
+start "DayZ-Expansion Server" /wait "DayZServer_x64.exe" -config=serverDZ.cfg -port=2402 "-mod=mods\@CF;mods\@Community-Online-Tools;mods\@DayZ-Expansion;mods\@DayZ-Expansion-Licensed;mods\@Trader;mods\@TraderWalletCollection" -profiles=.\ServerProfile -netlog -adminlog -enableHT -malloc=tbbmalloc 
 echo (%time%) %watch% wurde beendet! DayZ Server wird neu gestartet...
 goto watchdog
